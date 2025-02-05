c96d76c6e27be5d7c6b9f54b7c147c7ca6d97422
RitikMittal32 <122687970+RitikMittal32@users.noreply.github.com>
2025-02-05 16:41:53 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -47,7 +47,7 @@ export default function Header({selectedPage, setSelectedPage}){
                                                   <div className='line'></div>
                                                   </div>
                                                   </a>
-                              <a href='https://drive.google.com/file/d/1WMLcG4RoV0-SZKJODvp62l2ukiWWjmG2/view?usp=sharing' target='_blank' onClick={() => setSelectedPage('resume')}>
+                              <a href='https://drive.google.com/file/d/1w7er9nratfDbbiWAiNNBOb5wxbL9JC06/view?usp=sharing' target='_blank' onClick={() => setSelectedPage('resume')}>
 
                                                   <div className='heading w'><p>{'<'}Resume{'/>'}</p>
                                                   <div className='line'></div>
